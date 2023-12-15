4950c5610daf0f9d948946cf79e09503d923bdcd
devTanli <109398946+Ol12323@users.noreply.github.com>
2023-12-15 16:45:44 +0800

    Update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,13 @@ import laravel from 'laravel-vite-plugin'
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/sass/app.scss', 'resources/js/app.js', 'resources/css/filament/admin/theme.css', 'resources/css/filament/customer/theme.css'],
+            input: [
+             'resources/sass/app.scss',
+             'resources/js/app.js',
+             'resources/css/filament/admin/theme.css',
+             'resources/css/filament/customer/theme.css',
+             'resources/css/app.css',
+            ],
             refresh: true,
         }),
     ],
