71b31524656a7cd270d50c169e41daac59c30fa3
devTanli <109398946+Ol12323@users.noreply.github.com>
2023-12-15 18:07:36 +0800

    Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,7 +29,7 @@
         <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
           <h2 class="text-2xl font-semibold tracking-tight text-white">Featured services</h2>
           <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          @if ($feautured->isNotEmpty())
+          @if ($featured->isNotEmpty())
             @foreach ($featured as $item)
             <div class="group relative">
               <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
