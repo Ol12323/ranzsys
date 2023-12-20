f5138d80afcb17098e7b581907326fd76b45bbaa
Ol12323 <stanley.dalumpines@gmail.com.com>
2023-12-20 12:08:39 +0800

    Add user model and migration

---
+++ database/migrations/2023_12_20_025521_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('last_name');
+            $table->string('first_name');
+            $table->text('avatar');
+            $table->string('phone_number');
+            $table->date('date_of_birth');
+            $table->text('address');
+            $table->unsignedBigInteger('role_id');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->boolean('is_banned')->default(false);
+            $table->timestamps();
+
+            $table->foreign('role_id')->references('id')->on('roles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
