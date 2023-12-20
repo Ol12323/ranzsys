7b9f3951ecae9f4c0662822e9165112ab51d64e7
Ol12323 <stanley.dalumpines@gmail.com.com>
2023-12-20 19:34:01 +0800

    Role seeder

---
+++ database/seeders/CreateRoles.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Role;
+
+class CreateRoles extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $role = [
+            [
+                'name' => 'Owner',
+                'description' => 'Manage sales and services',
+            ],
+
+            [
+                'name' => 'Customer',
+                'description' => 'Manage sales',
+            ],
+
+            [
+                'name' => 'Customer',
+                'description' => 'Purchase services',
+            ],
+        ];
+
+        foreach($roles as $newRole){
+            Role::create($newRole);
+        }
+    }
+}
