4d43ae0ab057b6d00e2b4954c783228d49c223e2
Ol12323 <stanley.dalumpines@gmail.com.com>
2023-12-21 10:57:18 +0800

    Users seeder

---
+++ database/seeders/CreateUsers.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Role;
+use App\Models\User;
+
+class CreateUsers extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $roles = Role::pluck('id')->toArray();
+
+        User::create([
+            'last_name' => 'Dalumpines',
+            'first_name' => 'Stanley',
+            'avatar' => 'default.png',
+            'phone_number' => '09514093271',
+            'date_of_birth' => '2001-11-02',
+            'address' => 'Homelabd subd., Dapco, Panabo city',
+            'role_id' => $roles[1],
+            'email' => 'stanley.dalumpines@gmail.com',
+            'password' => bcrypt('password'),
+            'is_banned' => false,
+        ]);
+    }
+}
