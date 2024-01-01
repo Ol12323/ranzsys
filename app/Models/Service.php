c4fec9a348355236f4760091f54a0d60b50bcdd7
devTanli <109398946+Ol12323@users.noreply.github.com>
2024-01-01 09:19:38 +0800

    Service category namespace

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-
+use App\Models\ServiceCategory;
 
 class Service extends Model
 {
